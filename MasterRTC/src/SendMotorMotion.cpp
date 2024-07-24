f25f134af4f8f75067d534668fe7ef06221164f2
m0-ozakirintaro <72505537+m0-ozakirintaro@users.noreply.github.com>
2024-07-24 18:16:40 +0900

    最終発表

--- MasterRTC/src/SendMotorMotion.cpp
+++ MasterRTC/src/SendMotorMotion.cpp
@@ -15,28 +15,28 @@ SendMotorMotion::~SendMotorMotion()
 void SendMotorMotion::Grab_Ball_Position_1(uint16_t* motor_data)
 {
 	motor_data[0] = 0; //send_arg[0];
-	motor_data[1] = 60; //send_arg[1];
+	motor_data[1] = -130; //send_arg[1];
 	motor_data[2] = 0;//send_arg[2];
-	motor_data[3] = 80;//send_arg[3];
-	motor_data[4] = 180;//send_arg[4];
+	motor_data[3] = 120;//send_arg[3];
+	motor_data[4] = -150;//send_arg[4];
 }
 
 void SendMotorMotion::Grab_Ball_Position_2(uint16_t* motor_data)
 {
 	motor_data[0] = 0; //send_arg[0];
-	motor_data[1] = -10; //send_arg[1];
-	motor_data[2] = -60;//send_arg[2];
+	motor_data[1] = -130; //send_arg[1];
+	motor_data[2] = 0;//send_arg[2];
 	motor_data[3] = 80; //send_arg[3];
-	motor_data[4] = 180;//send_arg[4];
+	motor_data[4] = -150;//send_arg[4];
 }
 
 void SendMotorMotion::Grab_Ball_Position_3(uint16_t* motor_data)
 {
 	motor_data[0] = 0; //send_arg[0];
-	motor_data[1] = -20; //send_arg[1];
-	motor_data[2] = -100;//send_arg[2];
-	motor_data[3] = 150; //send_arg[3];
-	motor_data[4] = 100;//send_arg[4];
+	motor_data[1] = -130; //send_arg[1];
+	motor_data[2] = 0;//send_arg[2];
+	motor_data[3] = 120; //send_arg[3];
+	motor_data[4] = -150;//send_arg[4];
 }
 void SendMotorMotion::Grab_Ball(uint16_t* motor_data)
 {
